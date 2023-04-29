2c282ffad4dae747f841835c798dea55aa6cdebe
Fredrik Vullum <vullum.fredrik@gmail.com>
2023-04-29 16:12:34 +0200

    Logging now disabled by default

--- SteamConnectionInfo.Core/Models/Configuration.cs
+++ SteamConnectionInfo.Core/Models/Configuration.cs
@@ -9,12 +9,12 @@
         public double WindowPositionX { get; set; } = 960;
         public double WindowPositionY { get; set; } = 540;
         public int WindowOpacity { get; set; } = 80;
-        public bool ColumnIpEnabled { get; set; } = true;
+        public bool ColumnIpEnabled { get; set; } = false;
         public bool ColumnPingEnabled { get; set; } = true;
         public bool ColumnRelayEnabled { get; set; } = false;
         public bool ColumnCountryEnabled { get; set; } = true;
         public bool ColumnNameEnabled { get; set; } = true;
-        public bool LoggingEnabled { get; set; } = true;
+        public bool LoggingEnabled { get; set; } = false;
         public bool PingFilterEnabled { get; set; } = false;
         public bool CountryFilterEnabled { get; set; } = false;
 
